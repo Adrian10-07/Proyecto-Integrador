733d4b9d72cc40faa2dbdba912ca7e0d9a13fed0
Adrián10-07 <adrian2aguilarg@gmail.com>
2024-07-08 02:05:31 -0600

    helloword

--- moz-todo-react/src/main.jsx
+++ moz-todo-react/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './componentes/LoginForm/Login';
 import Inicio from './componentes/InicioForm/Inicio';
 import Usuarios from './componentes/UsuariosForm/Usuarios';
 import Alumnos from './componentes/AlumnosForm/Alumnos';
