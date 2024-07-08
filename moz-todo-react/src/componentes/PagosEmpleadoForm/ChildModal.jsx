e039367296ef66107f21848cdb65fe305aa0bb9d
Adrián10-07 <adrian2aguilarg@gmail.com>
2024-07-08 01:42:33 -0600

    hola mundo

--- moz-todo-react/src/componentes/PagosEmpleadoForm/ChildModal.jsx
+++ moz-todo-react/src/componentes/PagosEmpleadoForm/ChildModal.jsx
@@ -3,6 +3,19 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
 function ChildModal() {
   const [open, setOpen] = React.useState(false);
