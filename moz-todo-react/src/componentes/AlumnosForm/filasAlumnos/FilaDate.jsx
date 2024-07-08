4e402450e18d3fdd807c801b33b924ac79fddecf
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2024-07-07 21:15:10 -0600

    Intento pobre de hacer modales

--- moz-todo-react/src/componentes/AlumnosForm/filasAlumnos/FilaDate.jsx
+++ moz-todo-react/src/componentes/AlumnosForm/filasAlumnos/FilaDate.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { SiGoogleforms } from "react-icons/si";
+import { MdEdit } from "react-icons/md";
+import DataAlumno from "../ModalesForm/DataAlumno/DataAlumno";
 
 export default function FilaDate (
     {idAlumno, noControlAlumno, nombreAlumno, apellidoPAlumno, 
         apellidoMAlumno, gradoAlumno, grupoAlumno, turnoAlumno, estatusAlumno}){
+    
+    const [show, setShow] = useState(false);
+
+    const showModal = () => {
+        setShow(true);
+    }
+
+    const hideModal = () => {
+        setShow(false);
+    }
+
     return(
         <tr key={idAlumno}>
             <td>{noControlAlumno}</td>
@@ -13,6 +27,9 @@ export default function FilaDate (
             <td>{grupoAlumno}</td>
             <td>{turnoAlumno}</td>
             <td>{estatusAlumno}</td>
+            <td><button type="button" onClick={showModal}><SiGoogleforms/></button></td>
+            <td><button><MdEdit/></button></td>
+            <DataAlumno/>
         </tr>
     );
 }
